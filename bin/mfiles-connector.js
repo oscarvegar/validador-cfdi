b6b2fca63324e3c7c9e4d0d88a1563110a70563b
oscar.vega <oscar.vega@atomicware.mx>
2015-05-24 23:38:29 -0500

    cambios hacia nueva bóveda y muchas validaciones

--- bin/mfiles-connector.js
+++ bin/mfiles-connector.js
@@ -11,7 +11,7 @@ var auth = null;
 client.registerMethod("authenticationtokens",mfilesCfg.mfilesConfig.authenticationTokensURL,"POST");
 client.registerMethod("createObject",mfilesCfg.mfilesConfig.createObjectURL,"POST");
 client.registerMethod("files",mfilesCfg.mfilesConfig.uploadFilesURL,"POST");
-client.registerMethod("createVendor",mfilesCfg.mfilesConfig.createVendorURL,"POST");
+client.registerMethod("createVendor",mfilesCfg.mfilesConfig.createVendorURL+mfilesCfg.mfilesConfig.customerList,"POST");
 
 console.log("OBSERVANDO CARPETA logic/valid")
 watch('../logic/valid/', {recursive: false},function(filename) {
@@ -24,7 +24,9 @@ watch('../logic/valid/', {recursive: false},function(filename) {
 	  headers:{"Content-Type": "application/json"} 
 	};
 	args.data = mfilesCfg.mfilesConfig.loginInfo;
+
 	client.methods.authenticationtokens(args,function(data,response){
+
 		data = JSON.parse(data);
 		auth = data.Value;
 		var singleFilename = filename.replace("../logic/valid/","").replace(".xml","");
@@ -43,23 +45,21 @@ watch('../logic/valid/', {recursive: false},function(filename) {
   			var uuid = result['cfdi:Comprobante']['cfdi:Complemento'][0]['tfd:TimbreFiscalDigital'][0]['$']['UUID'];
   			var folio = result['cfdi:Comprobante']['$']['folio'];
   			var vendorName = result['cfdi:Comprobante']['cfdi:Emisor'][0]['$']["nombre"];
-			console.log("LLAMA A FILES: ")
-			//console.log(args)
+  			var domFiscal = result['cfdi:Comprobante']['cfdi:Emisor'][0]['cfdi:DomicilioFiscal'][0]['$'];
+  			var rfcReceptor = result['cfdi:Comprobante']['cfdi:Receptor'][0]['$']['rfc'];
 			client.methods.files(args,function(data,response){
-				console.log(data);
 				xmlFILE = JSON.parse(data);
 				var existePDF = fs.existsSync("../logic/pdf/"+singleFilename+".pdf");
 				if(existePDF){
-					unirest.post('http://mfiles-gi.cloudapp.net/REST/files')
+					unirest.post(mfilesCfg.mfilesConfig.uploadFilesURL)
 					.headers({"Content-Type": "application/octet-stream","X-Authentication":auth})
 					.attach('file', "../logic/pdf/"+singleFilename+".pdf") // Attachment
 					.end(function (response) {
-						console.log(response.body)
 					  pdfFILE = response.body;
-					  createObject(serie,rfcEmisor,uuid,folio,xmlFILE,txtFILE,pdfFILE,fileRes,singleFilename,vendorName)
+					  createObject(serie,rfcEmisor,uuid,folio,xmlFILE,txtFILE,pdfFILE,fileRes,singleFilename,vendorName,domFiscal,rfcReceptor)
 					});
 				}else{
-					createObject(serie,rfcEmisor,uuid,folio,xmlFILE,txtFILE,null,fileRes,singleFilename,vendorName)
+					createObject(serie,rfcEmisor,uuid,folio,xmlFILE,txtFILE,null,fileRes,singleFilename,vendorName,domFiscal,rfcReceptor)
 				}
 			})
 
@@ -68,22 +68,20 @@ watch('../logic/valid/', {recursive: false},function(filename) {
 	});
 });
 
-function createObject(serie,rfcEmisor,uuid,folio,xmlFILE,txtFILE,pdfFILE,fileRes,singleFilename,vendorName){
+function createObject(serie,rfcEmisor,uuid,folio,xmlFILE,txtFILE,pdfFILE,fileRes,singleFilename,vendorName,domFiscal,rfcReceptor){
 	args = {
 			headers:{"Content-Type": "application/octet-stream","X-Authentication":auth},
 			data:fileRes
 	};
 	client.methods.files(args,function(data,response){
 		var fileObject = JSON.parse(fileRes);
-		console.log(data);
 		txtFILE = JSON.parse(data);
 		args = {
 	 		headers:{"Content-Type": "application/octet-stream","X-Authentication":auth}
 		};
-		client.get("http://mfiles-gi.cloudapp.net/REST/objects/"+mfilesCfg.mfilesConfig.customerList+"?q="+rfcEmisor, args, function(datosProveedor, responses){
+		client.get(mfilesCfg.mfilesConfig.queryObject+mfilesCfg.mfilesConfig.customerList+"?q="+rfcEmisor, args, function(datosProveedor, responses){
 			datosProveedor = JSON.parse(datosProveedor);
 			var proveedorID = null;
-			//console.log(datosProveedor)
 			var filesUP = [{UploadID:xmlFILE.UploadID,Title:singleFilename,Extension:"xml",Size:xmlFILE.Size},
 		            		{UploadID:txtFILE.UploadID,Title:singleFilename,Extension:"txt",Size:txtFILE.Size}];
 	        if(pdfFILE!=null){
@@ -96,41 +94,40 @@ function createObject(serie,rfcEmisor,uuid,folio,xmlFILE,txtFILE,pdfFILE,fileRes
 	        	respuestaWS = mfilesCfg.mfilesConfig.estadoSat.INVALIDO 
 	        else if(fileObject.status === -2)
 	        	respuestaWS = mfilesCfg.mfilesConfig.estadoSat.NOENCONTRADO 
+	        console.info("singleFilename",singleFilename)
 	        args = {
 		 		headers:{"Content-Type": "application/json","X-Authentication":auth},
 		 		data:{
 		            PropertyValues: [{
-		                    // Document name
 		                    PropertyDef: 0,
 		                    TypedValue: { DataType: 1, Value: singleFilename }
 		                }, {
-		                    // "Single File" property
 		                    PropertyDef: 22,
 		                    TypedValue: { DataType: 8, Value: false }
 		                }, {
-		                    // Class.
 		                    PropertyDef: 100,
 		                    TypedValue: { DataType: 9, Lookup: { Item: mfilesCfg.mfilesConfig.clase } }
 		                },{
-		                    // NUMERO FACTURA.
 		                    PropertyDef: mfilesCfg.mfilesConfig.numeroFactura,
-		                    TypedValue: { DataType: 1, Value: folio }
+		                    TypedValue: { DataType: 2, Value: parseInt(folio) }
 		                },{
-		                    // RFC.
 		                    PropertyDef: mfilesCfg.mfilesConfig.rfc,
 		                    TypedValue: { DataType: 1, Value: rfcEmisor }
 		                },{
-		                    // SERIE.
 		                    PropertyDef: mfilesCfg.mfilesConfig.serie,
 		                    TypedValue: { DataType: 1, Value: serie }
 		                },{
-		                    // UID.
 		                    PropertyDef: mfilesCfg.mfilesConfig.uid,
 		                    TypedValue: { DataType: 1, Value: uuid }
 		                },{
-		                    // ESTADO SAT.
 		                    PropertyDef: mfilesCfg.mfilesConfig.estadoSatPD,
 		                    TypedValue: { DataType: 9, Lookup: { Item: respuestaWS }}
+		                },{
+		                    PropertyDef: mfilesCfg.mfilesConfig.rfcReceptor,
+		                    TypedValue: { DataType: 1, Value: rfcReceptor }
+		                },{
+		                    PropertyDef: mfilesCfg.mfilesConfig.fechaDocto,
+		                    TypedValue: { DataType: 5, Value: new Date() }
 		                }],
 		            Files: filesUP
 	        	}
@@ -138,11 +135,14 @@ function createObject(serie,rfcEmisor,uuid,folio,xmlFILE,txtFILE,pdfFILE,fileRes
 	        if(datosProveedor.Items.length>0){
 	        	proveedorID = datosProveedor.Items[0].ObjVer.ID;
 				args.data.PropertyValues.push({PropertyDef: mfilesCfg.mfilesConfig.customer,
-		                    TypedValue: { DataType: 10, Lookups: [{ Item: proveedorID }]  }
+		                    TypedValue: { DataType: 9, Lookup: { Item: proveedorID }  }
 		                });
 
 				client.methods.createObject(args,function(data,response){
-					console.log(data);
+			    		if(response.statusCode==200)
+							console.info("***** SE CREO NUEVA FACTURA MFILES *****",data)
+						else
+							console.error("XXXXX OCURRIO UN ERROR EN LA CREACION DE FACTURA XXXXX",data)
 				});
 			}else{
 				var argsProv = {
@@ -150,35 +150,76 @@ function createObject(serie,rfcEmisor,uuid,folio,xmlFILE,txtFILE,pdfFILE,fileRes
 					data:{
 					    PropertyValues: [
 					    	{
-			                    // Class.
 			                    PropertyDef: 100,
-			                    TypedValue: { DataType: 9, Lookup: { Item: 98 } }
+			                    TypedValue: { DataType: 9, Lookup: { Item: mfilesCfg.mfilesConfig.claseProveedor } }
 			                },
 					    	{
-					            // RFC
-						        PropertyDef: 1164,
+						        PropertyDef: mfilesCfg.mfilesConfig.rfc,
 						        TypedValue: { DataType: 1, Value:rfcEmisor }
 					    	},
 					    	{
-					            // Corporate?
-						        PropertyDef: 1183,
+						        PropertyDef: mfilesCfg.mfilesConfig.corporativo,
 						        TypedValue: { DataType: 8, Value:true }
 					    	},
 					    	{
-					            // Name
-						        PropertyDef: 1110,
+						        PropertyDef: mfilesCfg.mfilesConfig.vendorName,
 						        TypedValue: { DataType: 1, Value:vendorName}
+					    	},
+					    	{
+						        PropertyDef: mfilesCfg.mfilesConfig.calleProveedor,
+						        TypedValue: { DataType: 1, Value:domFiscal.calle?domFiscal.calle:"NA"}
+					    	},
+					    	{
+						        PropertyDef: mfilesCfg.mfilesConfig.noExteriorProveedor,
+						        TypedValue: { DataType: 1, Value:domFiscal.noExterior?domFiscal.noExterior:"NA"}
+					    	},
+					    	{
+						        PropertyDef: mfilesCfg.mfilesConfig.noInteriorProveedor,
+						        TypedValue: { DataType: 1, Value:domFiscal.noInterior?domFiscal.noInterior:"NA"}
+					    	},
+					    	{
+						        PropertyDef: mfilesCfg.mfilesConfig.coloniaProveedor,
+						        TypedValue: { DataType: 1, Value:domFiscal.colonia?domFiscal.colonia:"NA"}
+					    	},
+					    	{
+						        PropertyDef: mfilesCfg.mfilesConfig.paisProveedor,
+						        TypedValue: { DataType: 1, Value:domFiscal.pais?domFiscal.pais:"NA"}
+					    	},
+					    	{
+						        PropertyDef: mfilesCfg.mfilesConfig.estadoProveedor,
+						        TypedValue: { DataType: 1, Value:domFiscal.estado?domFiscal.estado:"NA"}
+					    	},
+					    	{
+						        PropertyDef: mfilesCfg.mfilesConfig.ciudadProveedor,
+						        TypedValue: { DataType: 1, Value:domFiscal.municipio?domFiscal.municipio:"NA"}
+					    	},
+					    	{
+						        PropertyDef: mfilesCfg.mfilesConfig.localidadProveedor,
+						        TypedValue: { DataType: 1, Value:domFiscal.localidad?domFiscal.localidad:"NA"}
+					    	},
+					    	{
+						        PropertyDef: mfilesCfg.mfilesConfig.codigoPostalProveedor,
+						        TypedValue: { DataType: 1, Value:domFiscal.codigoPostal?domFiscal.codigoPostal:"NA"}
 					    	}]
 			    	}
 			    }
 			    client.methods.createVendor(argsProv,function(data,response){
-			    	proveedorID = JSON.parse(data).ObjVer.ID;
-			    	args.data.PropertyValues.push({PropertyDef: mfilesCfg.mfilesConfig.customer,
-	                    TypedValue: { DataType: 10, Lookups: [{ Item: proveedorID }]  }
-	                });
-			    	client.methods.createObject(args,function(data,response){
-						console.log(data);
-					});
+			    	if(response.statusCode==200){
+			    		console.info("***** SE CREO NUEVO PROVEEDOR *****",data)
+				    	proveedorID = JSON.parse(data).ObjVer.ID;
+				    	args.data.PropertyValues.push({PropertyDef: mfilesCfg.mfilesConfig.customer,
+		                    TypedValue: { DataType: 9, Lookup: { Item: proveedorID }  }
+		                });
+				    	client.methods.createObject(args,function(data,response){
+				    		if(response.statusCode==200)
+								console.info("***** SE CREO NUEVA FACTURA MFILES *****",data)
+							else
+								console.error("XXXXX OCURRIO UN ERROR EN LA CREACION DE FACTURA XXXXX",data)
+						});
+			    	}else{
+			    		console.error("XXXXX OCURRIO UN ERROR EN LA CREACION DEL PROVEEDOR XXXXX",data)
+			    	}
+			    	
 			    })
 			}
 	        
--- config/mfiles-cfg.js
+++ config/mfiles-cfg.js
@@ -1,17 +1,55 @@
 exports.mfilesConfig = {
-  	clase:113,
-  	customer:1079,
-  	//customer:1110,
-  	numeroFactura:1155,
-  	rfc:1164,
-  	serie:1192,
-  	uid:1193,
-  	estadoSatPD:1195,
+    /* CLASE DE LA FACTURUA */
+    clase:2,
+    /* FECHA DE LA FACTURUA */
+    fechaDocto:1002,
+    /* PROPIEDAD DE PROVEEDOR */
+  	customer:1212,
+  	/* FOLIO DE LA FACTURA */
+  	numeroFactura:1206,
+    /* RFC EMISOR */
+    rfc:1164,
+    /* RFC RECEPTOR */
+    rfcReceptor:1222,
+    /* SERIE DE LA FACTURA */
+  	serie:1205,
+    /* UID DE LA FACTURA */
+  	uid:1204,
+    /*ID DEL VALUE LIST DE ESTADOS DE SAT*/
+    estadoSatPD:1209,
+    /*PROPIEDAD CORPORATIVO DE LA CLASE PROVEEDOR*/
+    corporativo:1183,
+    /*PROPIEDAD NOMBRE DEL PROVEEDOR*/
+    vendorName:1110,
+    /*CLASE DEL PROVEEDOR*/
+    claseProveedor:98,
+    /*CALLE DEL PROVEEDOR*/
+    calleProveedor:1217,
+    /*NO EXTERIOR DEL PROVEEDOR*/
+    noExteriorProveedor:1218,
+    /*NO INTERIOR DEL PROVEEDOR*/
+    noInteriorProveedor:1219,
+    /*COLONIA DEL PROVEEDOR*/
+    coloniaProveedor:1201,
+    /*CODIGOPOSTAL DEL PROVEEDOR*/
+    codigoPostalProveedor:1087,
+    /*LOCALIDAD DEL PROVEEDOR*/
+    localidadProveedor:1202,
+    /*CIUDAD DEL PROVEEDOR*/
+    ciudadProveedor:1216,
+    /*ESTADO DEL PROVEEDOR*/
+    estadoProveedor:1214,
+    /*PAIS DEL PROVEEDOR*/
+    paisProveedor:1213,
+    /*ID DEL VALUE LIST DE PROVEEDORES*/
+    customerList:136,
+    /*POSIBLES ESTADOS DEL SAT */
   	estadoSat:{OK:1,INVALIDO:2,NOENCONTRADO:3},
-  	customerList:136,
-    loginInfo : { username: "Administrator",password:"Intecsa12*",VaultGuid: "59E3E2C4-3B7A-4034-B8F6-88DCF894F2B6" },
+    loginInfo : { username: "Ovega",password:"Intecsa12*",VaultGuid: "3A7EFDC2-BF4C-4CCB-8994-FC5D59606F56" },
+    //loginInfo : { username: "Administrator",password:"Intecsa12*",VaultGuid: "59E3E2C4-3B7A-4034-B8F6-88DCF894F2B6" },
     authenticationTokensURL:"http://mfiles-gi.cloudapp.net/REST/server/authenticationtokens",
     createObjectURL:"http://mfiles-gi.cloudapp.net/REST/objects/0",
-    createVendorURL:"http://mfiles-gi.cloudapp.net/REST/objects/136",
-    uploadFilesURL:"http://mfiles-gi.cloudapp.net/REST/files"    
+    createVendorURL:"http://mfiles-gi.cloudapp.net/REST/objects/",
+    uploadFilesURL:"http://mfiles-gi.cloudapp.net/REST/files",   
+    queryObject:"http://mfiles-gi.cloudapp.net/REST/objects/" 
 };
